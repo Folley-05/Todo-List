aa035642ae707d9c157800d9c2ce19089f956257
Folley-05 <aristotepascaldjounda@gmail.com>
2020-10-10 05:51:08 +0100

    modification de la page d'accueil

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Composant from './component';
+
+function App() {
+  return (
+    <div className="App">
+        <Composant />
+    </div>
+  );
+}
+
+export default App;
